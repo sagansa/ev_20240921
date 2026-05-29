b4e108770787f87fa05385565650ba9d03a74c2a
sagansa <asapanganbangsa@gmail.com>
2026-05-29 19:17:25 +0700

    Update ChargeResource.php

--- app/Filament/Resources/Panel/ChargeResource.php
+++ app/Filament/Resources/Panel/ChargeResource.php
@@ -15,7 +15,7 @@ use App\Models\Charge;
 use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
