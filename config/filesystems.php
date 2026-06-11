cf229271959a3b31624dddb892be8e2094e1a7aa
sagansa <asapanganbangsa@gmail.com>
2026-06-11 14:04:26 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,12 +36,21 @@ return [
             'throw' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => env('FILESYSTEM_PUBLIC_ROOT', storage_path('app/public')),
+            'url' => rtrim(env('FILESYSTEM_PUBLIC_URL', env('APP_URL', 'http://localhost').'/storage'), '/'),
             'visibility' => 'public',
             'throw' => false,
+            'report' => false,
         ],
 
         's3' => [
