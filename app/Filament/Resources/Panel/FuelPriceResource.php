465b290a92352daee3f61db0e6ffc42b8818c1ed
sagansa <asapanganbangsa@gmail.com>
2026-08-07 00:32:09 +0700

    Update FuelPriceResource.php

--- app/Filament/Resources/Panel/FuelPriceResource.php
+++ app/Filament/Resources/Panel/FuelPriceResource.php
@@ -9,7 +9,7 @@ use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use App\Models\FuelPrice;
 use Filament\Resources\Resource;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
